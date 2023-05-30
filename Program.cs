cc08b2c8f29b39e1f6af64f897b1dca4873b906c
Kimosable <129333154+Kimosable@users.noreply.github.com>
2023-05-30 15:35:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Write("Введите m массива: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите n массива: ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m, n];
+Random rand = new Random();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = rand.NextDouble() * 200 - 100;
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{array[i, j]:F2}\t");
+    }
+    Console.WriteLine();
+}
